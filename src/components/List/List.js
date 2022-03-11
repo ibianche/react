0d12ccd232cb5cf2f75e083189de9ff0451410ec
ibianche <inna.pomorska@gmail.com>
2022-03-11 18:37:15 +0100

    class Hero, List

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './List.scss';
+import Hero from '../Hero/Hero';
 
 class List extends React.Component {
   render() {
     return (
       <section className={styles.component}>
-      <h2 className={styles.subtitle}>Hello!!!</h2>
+      <h2 className={styles.subtitle}><Hero /></h2>
     </section>
   )
   }
