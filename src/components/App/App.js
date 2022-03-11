0db9ff22515d595965e09ba611aa798e96546712
ibianche <inna.pomorska@gmail.com>
2022-03-11 17:12:53 +0100

    <main>

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,10 +3,12 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div id="app">
+      <main className="app-content">
         <h1>My first React app</h1>
-    <h2>Hello world!</h2>
-      </div>
+        <h2>Hello world!</h2>
+      </main>
+    </div>
     )
   }
 }
