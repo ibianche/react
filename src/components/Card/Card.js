6a26baaa6b14221722737e100c647765a5875c13
ibianche <inna.pomorska@gmail.com>
2022-03-18 16:57:48 +0100

    list.js

---
+++ src/components/Card/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Card.scss';
+import {settings} from '../../data/dataStore';
+import List from "../List/List";
+
+class Card extends React.Component {
+  render() {
+    return(
+      <section className={styles.component}>
+      </section>
+
+    )
+  }
+}
+export default Card;
