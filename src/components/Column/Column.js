0301fff757d76121cd1dcd5d94d453bea57bd8e8
ibianche <inna.pomorska@gmail.com>
2022-03-23 23:08:36 +0100

    cards

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,10 +2,17 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import List from '../List/List';
+import {settings} from '../../data/dataStore';
+import Card from '../Card/Card';
+import Creator from '../Creator/Creator';
 
 
 
 class Column extends React.Component {
+  state = {
+    cards: this.props.columns || [],
+  }
+
   static propTypes = {
     title: PropTypes.node.isRequired,
   }
@@ -14,9 +21,37 @@ class Column extends React.Component {
     return(
       <main className={styles.component}>
         <h3 className={styles.title}>{this.props.title}</h3>
+
+        <div className={styles.cards}>
+          {this.state.cards.map(({key, ...cardsProps}) => (
+            <Card key={key} {...cardsProps} />
+          ))}
+        </div>
+
+        <div className={styles.creator}>
+          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
+        </div>
       </main>
     )
   }
+
+  addCard(title){
+    this.setState(state => (
+      {
+        cards: [
+          ...state.cards,
+          {
+            key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
+            title,
+            icon: 'list-alt',
+            cards: []
+          }
+        ]
+      }
+    ));
+  }
+
+
 }
 
 export default Column;
