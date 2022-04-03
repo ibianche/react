225412e1bab713733a5653a1299a3ff6da55ca37
ibianche <inna.pomorska@gmail.com>
2022-04-03 11:30:19 +0200

    Modification in list.js task 16.3

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -7,10 +7,6 @@ import {settings} from '../../data/dataStore';
 import Creator from '../Creator/Creator';
 
 class List extends React.Component {
-  state = {
-    columns: this.props.columns || [],
-  };
-
   static propTypes = {
     title: PropTypes.node.isRequired,
     description: PropTypes.node,
@@ -23,39 +19,31 @@ class List extends React.Component {
   };
 
   render() {
+    const {title, image, description} = this.props;
     return (
       <section className={styles.component}>
-        <h2 className={styles.subtitle}><Hero titleText={this.props.title} imageBackground={this.props.image}/></h2>
+        <h2 className={styles.subtitle}><Hero titleText={title} image={image} />
+          <div className={styles.description}>
+            {ReactHtmlParser(description)}
+          </div>
         <div className={styles.description}>
           {this.props.description}
         </div>
-        <div className={styles.columns}>
-          {this.state.columns.map(({key, ...columnProps}) => (
-            <Column key={key} {...columnProps} />
-          ))}
-        </div>
-        <div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
-        </div>
+        {/*
+<div className={styles.columns}>
+  {this.state.columns.map(({key, ...columnProps}) => (
+    <Column key={key} {...columnProps} />
+  ))}
+</div>
+<div className={styles.creator}>
+  <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
+</div>
+*/}
       </section>
     );
   }
 
-  addColumn(title){
-    this.setState(state => (
-      {
-        columns: [
-          ...state.columns,
-          {
-            key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
-            title,
-            icon: 'list-alt',
-            cards: [],
-          },
-        ],
-      }
-    ));
-  }
+
 
 
 
