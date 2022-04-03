dd9aedbed3b651893a6d1d967b1c974e83d37725
ibianche <inna.pomorska@gmail.com>
2022-04-03 12:51:06 +0200

    task 16.3 step 1

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,8 +2,8 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import {settings} from '../../data/dataStore';
-import Card from '../Card/Card';
-import Creator from '../Creator/Creator';
+// import Card from '../Card/Card';
+// import Creator from '../Creator/Creator';
 import Icon from '../Icon/Icon';
 
 
@@ -20,6 +20,7 @@ class Column extends React.Component {
   };
 
   render() {
+    const {title, icon} = this.props;
     return(
       <main className={styles.component}>
         <h3 className={styles.title}>{this.props.title}
@@ -28,34 +29,34 @@ class Column extends React.Component {
           </span>
         </h3>
 
-        <div className={styles.cards}>
-          {this.state.cards.map(({key, ...cardsProps}) => (
-            <Card key={key} {...cardsProps} />
-          ))}
-        </div>
+        {/*<div className={styles.cards}>*/}
+          {/*{this.state.cards.map(({key, ...cardsProps}) => (*/}
+            {/*<Card key={key} {...cardsProps} />*/}
+          {/*))}*/}
+        {/*</div>*/}
 
-        <div className={styles.creator}>
-          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
-        </div>
+        {/*<div className={styles.creator}>*/}
+          {/*<Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>*/}
+        {/*</div>*/}
       </main>
     );
   }
 
-  addCard(title){
-    this.setState(state => (
-      {
-        cards: [
-          ...state.cards,
-          {
-            key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
-            title,
-            icon: 'list-alt',
-            cards: [],
-          },
-        ],
-      }
-    ));
-  }
+  // addCard(title){
+  //   this.setState(state => (
+  //     {
+  //       cards: [
+  //         ...state.cards,
+  //         {
+  //           key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
+  //           title,
+  //           icon: 'list-alt',
+  //           cards: [],
+  //         },
+  //       ],
+  //     }
+  //   ));
+  // }
 
 
 }
